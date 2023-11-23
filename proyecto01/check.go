12fe36226fa3d08d3c874683912dbba81fd025e8
Maximiliano Alias <damianalias1991@gmail.com>
2023-11-23 02:09:40 -0300

    CheakHealter Finalizado y testeado correctamente

---
+++ proyecto01/check.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+func Check(destination string, port string) string {
+	addres := destination + ":" + port
+	timeout := time.Duration(5 * time.Second)
+	conn, err := net.DialTimeout("tcp", addres, timeout)
+	var status string
+
+	if err != nil {
+		status = fmt.Sprintf("[DOWN] %v es accesible, \n Error: %v ", destination, port)
+	} else {
+		status = fmt.Sprintf("[UP] %v is no es accesible, \n Peticion desde IP: %v\n Para la IP: %v", destination, conn.LocalAddr(), conn.RemoteAddr())
+	}
+	return status
+}
--- proyecto01/main.go
+++ proyecto01/main.go
@@ -2,9 +2,51 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-
 	fmt.Println("hola mundo")
+
+	app := &cli.App{
+		Name:  "Healtcheker",
+		Usage: "La siguiente aplicación se usa para saber si un sitio web está online u offline",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "domain",
+				Aliases:  []string{"d"},
+				Usage:    "nombre del dominio a chequear.",
+				Required: true,
+			},
+			&cli.StringFlag{
+				Name:     "port",
+				Aliases:  []string{"p"},
+				Usage:    "Número de puerto a chequear.",
+				Required: false,
+			},
+		},
+
+		Action: func(c *cli.Context) error {
+			port := c.String("port")
+
+			if c.String("port") == "" {
+				port = "80"
+			}
+
+			status := Check(c.String("domain"), port)
+
+			fmt.Println(status)
+
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
